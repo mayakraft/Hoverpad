f99cd67a7a397308954e378b5f915530d6b4b057
robbykraft <robbykraft@gmail.com>
2014-03-08 17:33:04 -0800

    Initial Commit

---
+++ PilotPod/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PilotPod
+//
+//  Created by Robby Kraft on 3/8/14.
+//  Copyright (c) 2014 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PilotPod/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PilotPod
+//
+//  Created by Robby Kraft on 3/8/14.
+//  Copyright (c) 2014 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
